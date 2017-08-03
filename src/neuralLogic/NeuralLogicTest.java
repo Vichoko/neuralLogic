c4bc307e29a6f9d444eb00310d34dea9bf40484c
Vichoko <vicenteoyanedel@ing.uchile.cl>
2017-08-03 01:27:04 -0400

    sumador

--- src/neuralLogic/NeuralLogicTest.java
+++ src/neuralLogic/NeuralLogicTest.java
@@ -23,6 +23,15 @@ public class NeuralLogicTest {
 	Perceptron pNAND;
 	Perceptron pOR;
 
+	public int[] sum(int a, int b) throws Exception{
+		int outa = pNAND.synapsis(a, b);
+		int outb = pNAND.synapsis(a, outa);
+		int outc = pNAND.synapsis(outa, b);
+		int outd = pNAND.synapsis(outb, outc);
+		int oute = pNAND.synapsis(outa, outa);
+		return new int[] {outd, oute};
+		
+	}
 	@Before
 	public void setUp() throws Exception {
 		pAND = and();
@@ -32,6 +41,7 @@ public class NeuralLogicTest {
 
 	@Test
 	public void testAND() throws Exception {
+		// tabla de verdad
 		assertEquals(pAND.synapsis(0,0), 0);
 		assertEquals(pAND.synapsis(0,1), 0);
 		assertEquals(pAND.synapsis(1,0), 0);
@@ -52,6 +62,21 @@ public class NeuralLogicTest {
 		assertEquals(pOR.synapsis(0,1), 1);
 		assertEquals(pOR.synapsis(1,0), 1);
 		assertEquals(pOR.synapsis(1,1), 1);
+	}
+	@Test
+	public void testSum() throws Exception {
+		// primero es sum, segundo es carry
+		assertEquals(sum(0,0)[0], 0);
+		assertEquals(sum(0,0)[1], 0);
+		
+		assertEquals(sum(1,0)[0], 1);
+		assertEquals(sum(1,0)[1], 0);
+		
+		assertEquals(sum(0,1)[0], 1);
+		assertEquals(sum(0,1)[1], 0);
+		
+		assertEquals(sum(1,1)[0], 0);
+		assertEquals(sum(1,1)[1], 1);
 	}
 
 }
